36a647a9775d5cc2570f7c5c2c567ce4083629d5
TiboDev <the.titi72@gmail.com>
2015-10-13 16:49:47 +0300

    Modification test

--- Fingerprint sensor based identification/CCS/Fingerprint/main.c
+++ Fingerprint sensor based identification/CCS/Fingerprint/main.c
@@ -4,8 +4,10 @@
  * main.c
  */
 int main(void) {
+	int test = 0;
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
 	printf("La vie est cool !");
+	while(test)
+		printf("test again");
 	return 0;
-	// Tqoginwsognoijn
 }
