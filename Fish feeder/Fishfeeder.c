8dc51ce05c4ea9245ddb9a81916e6fe4c424f3f0
sinijarv <sander.sinijarv@gmail.com>
2015-11-06 13:10:57 +0200

    Added a function for light sensor
    Added the whole function for light sensor. It includes initializing the ADC for this purpose and converting the value to somewhat random number. The value where it is considered dark (or the lamp is broken) is taken from observations.

--- Fish feeder/Fishfeeder.c
+++ Fish feeder/Fishfeeder.c
@@ -74,10 +74,29 @@ void temperature(){
 
 }
 
-void lighting(){
+int lighting(int LightLevel){
+	
+	/*initialize ADC*/
+	ADC10CTL0 = ADC10IE + ADC10SHT_3 + ADC10ON;
+	ADC10CTL1 = INCH_0 + ADC10DIV_3;
 
+	__delay_cycles(5000);       	// settle time 30ms
 
-}
+	/*ADC conversion start*/
+	__delay_cycles(1000);           // Wait for reference to settle
+	ADC10CTL0 |= ENC + ADC10SC; 	// Sampling and conversion start
+	while(ADC10CTL1 & BUSY);    	// Wait..i am converting...
+	light = ADC10MEM;                    // Read ADC memory
+	light = light / 3;
+
+	while (!(IFG2&UCA0TXIFG));      // Wait until TX buffer is ready
+	UCA0TXBUF = light;
+	
+	if(light < 120){		//reading values from RealTerm
+		return 1;		//120 is a little brigther than ambient light
+	}				//the smaller the value, the brighter is light
+	else{
+		return 0;
 
 void waterlevel(){
 	
