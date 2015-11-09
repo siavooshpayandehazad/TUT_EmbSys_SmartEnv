3b19623673d9e2a49c34b8560ebec43b7ed13550
kraskaGM <martingrosberg@gmail.com>
2015-11-10 00:27:39 +0200

    4 functions added into the code
    //[week 11] [10.11.2015 00:24] Added function init_pins whisch is inizilizing the water level sensor pin as input
    // Added waterllevel function which is returning eather 1 or 0
    // Added Uart inizilation function which will start communication bridge between uart
    // Added Calibration function for clock -- main function is still empty because we have no idea how to arrange yet everything.

--- Fish feeder/Fishfeeder.c
+++ Fish feeder/Fishfeeder.c
@@ -62,6 +62,16 @@ int main(void) {
 	return 0;
 }
 
+void init_pins (void)
+{
+	P2SEL &=(~BIT0); //Set SEL P2.0 as GPIO
+	P2DIR &=(~BIT0); //input direction
+	P2IES |=(BIT0);
+	P2IFG  &=  (~BIT0);    //  Clear   interrupt   flag    for P2.0
+	P2IE   |=  (BIT0); //  Enable  interrupt   for P2.0
+}
+
+
 void timerA(){
 
 }
@@ -99,8 +109,15 @@ int lighting(int LightLevel){
 		return 0;
 		}
 	}
-void waterlevel(){
-	
+int waterlevel()
+{
+	if (P2IN == 1){
+		return 1;
+	}
+	else
+	{
+		return 0;	
+	}
 }
 
 void feedlevel(){
@@ -112,11 +129,33 @@ void motor(){
 }
 
 void UART(){
+	/*setting up UART*/
+	P1SEL  |= BIT1 + BIT2;  //Enable module function on pin
+	P1SEL2 |= BIT1 + BIT2;  //Select second module; P1.1 = RXD, P1.2=TXD
 
+	UCA0CTL1 |= UCSSEL_2;    // Set clock source SMCLK
+	UCA0BR0 = 104;           // 1MHz 9600 (N=BRCLK/baudrate)
+	UCA0BR1 = 0;             // 1MHz 9600
+	UCA0MCTL = UCBRS0;       // Modulation UCBRSx = 1
+	UCA0CTL1 &= ~UCSWRST;    // **Initialize USCI state machine**
 }
-
+void clockSetup1MHZ(void){
+	// Configure CPU clock
+	if (CALBC1_1MHZ==0xFF)		    // If calibration constant erased
+	{
+		while(1);                   // do not load, trap CPU
+	}
+	DCOCTL = 0;                     // Select lowest DCOx and MODx settings
+	BCSCTL1 = CALBC1_1MHZ;          // Set DCO to 1MHz
+	DCOCTL = CALDCO_1MHZ;
+	/* END OF DO NOT MODIFY */
+}
 /***************************************************************************************************
  *         History of changes                                                                       *
  ***************************************************************************************************/
 
 //[Week 8] Added empty function slots for future code.
+//[week 11] [10.11.2015 00:24] Added function init_pins whisch is inizilizing the water level sensor pin as input
+// Added waterllevel function which is returning eather 1 or 0
+// Added Uart inizilation function which will start communication bridge between uart
+// Added Calibration function for clock -- main function is still empty because we have no idea how to arrange yet everything.
